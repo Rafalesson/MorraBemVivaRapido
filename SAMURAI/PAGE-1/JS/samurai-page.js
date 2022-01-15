8a94c2caeef6015dff7d147573c8f4a459725a4d
Rafalesson <rafaalesson35@gmail.com>
2022-01-15 12:35:47 -0300

    Projeto do Menu do Game Morra Bem, Viva Rápido

---
+++ SAMURAI/PAGE-1/JS/samurai-page.js
@@ -0,0 +1,23 @@
+let mensagem
+
+function nomeDafunção() {
+
+    while (mensagem != 1 || mensagem != 2) {
+
+        mensagem = prompt("Digite 1 para AJUDAR \n Digite 2 para NÃ0 AJUDAR");
+
+        if (mensagem == 1) {
+            window.location.href = "C:/Users/thami/OneDrive/Documentos/Resilia/Projeto-Modulo-1/SAMURAI/AJUDOU/ajudou.html"
+            break;
+        }
+        else if (mensagem == 2) {
+            window.location.href = "C:/Users/thami/OneDrive/Documentos/Resilia/Projeto-Modulo-1/SAMURAI/NAO-AJUDOU/nao-ajudou.html";
+            break;
+        }
+        else {
+            alert('Digite 1 ou 2');
+            return nomeDafunção();
+        }
+    }
+
+}
